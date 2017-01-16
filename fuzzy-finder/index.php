1a7b9515621017016f00b7206900eae7e4c2fcef
Bas Hendriks <bashendriks14@msn.com>
2017-01-16 15:24:39 +0100

    remove container

--- fuzzy-finder/index.php
+++ fuzzy-finder/index.php
@@ -1,12 +1,10 @@
-<div class="container col-md-6 col-md-offset-3">
-	<label for="">Find your actor:</label>
-	<input type="text" class="form-control fuzzy">
+<label for="">Find your actor:</label>
+<input type="text" class="form-control fuzzy">
 
-	<div class="person" style="display: none">
-		<div class=""><b>Actors</b></div>
-		<ul class="list-group">
-		</ul>
-	</div>
+<div class="person" style="display: none">
+	<div class=""><b>Actors</b></div>
+	<ul class="list-group">
+	</ul>
 </div>
 
 <script src="https://cdnjs.cloudflare.com/ajax/libs/fuse.js/2.5.0/fuse.min.js"></script>
